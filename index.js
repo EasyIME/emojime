3497b7a53eecaa2549cda949f5de8df9c69bcb39
Lee <jessy1092@gmail.com>
2016-02-26 00:48:09 +0800

    feat: emojime implement quickly 🚀

--- index.js
+++ index.js
@@ -15,3 +15,82 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 'use strict';
+
+let nime = require('nime');
+let KEYCODE = require('nime/lib/keyCodes');
+let emojione = require('emojione');
+
+let server = nime.createServer();
+
+server.on('connection', (service) => {
+
+  let candidateList = [];
+  let showCandidates = false;
+  let compositionString = '';
+  let compositionCursor = 0;
+
+  service.on('filterKeyDown', (msg, keyHandler) => {
+
+    console.log('custom filterKeyDown');
+
+    let charCode = keyHandler.charCode;
+    let seqNum = msg['seqNum'];
+
+    let response = {
+      'return': false
+    };
+
+    if (compositionString !== '' || charCode === ':'.charCodeAt(0)) {
+      response['return'] = true;
+    }
+
+    service.writeSuccess(seqNum, response);
+  });
+
+  service.on('onKeyDown', (msg, keyHandler) => {
+    console.log('custom onKeyDown');
+
+    let keyCode = keyHandler.keyCode;
+    let charCode = keyHandler.charCode;
+    let seqNum = msg['seqNum'];
+
+    let response = {
+      'return': true
+    };
+
+    if (compositionString === '' && charCode === ':'.charCodeAt(0)) {
+      compositionString = ':';
+      compositionCursor = 1;
+
+      response['compositionString'] = compositionString;
+      response['compositionCursor'] = compositionCursor;
+
+    } else if (compositionString !== '') {
+
+      if (charCode === ':'.charCodeAt(0)) {
+        let emojikey = compositionString + ':';
+        compositionString = '';
+        compositionCursor = 0;
+
+        console.log('Get emoji short name');
+        console.log(emojikey);
+        console.log(emojione.shortnameToUnicode(emojikey));
+        response['commitString'] = emojione.shortnameToUnicode(emojikey);
+        response['compositionString'] = '';
+
+      } else if (
+        (charCode >= 'a'.charCodeAt(0) && charCode <= 'z'.charCodeAt(0)) ||
+        (charCode >= 'A'.charCodeAt(0) && charCode <= 'Z'.charCodeAt(0))) {
+
+        compositionString += String.fromCharCode(charCode);
+        compositionCursor += 1;
+      }
+      response['compositionString'] = compositionString;
+      response['compositionCursor'] = compositionCursor;
+
+    }
+    service.writeSuccess(seqNum, response);
+  });
+});
+
+server.listen();
